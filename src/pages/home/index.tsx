c1a1df792dd81e1ffd775395b7b71475e53cf057
Pedro Paulo <pedropaulotj@gmail.com>
2022-04-14 00:02:03 -0300

    Ajustes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ButtonOptions } from "../../components/ButtonOptions";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
@@ -7,7 +7,10 @@ import { Textarea } from "../../components/Textarea";
 import styles from "./styles.module.scss";
 
 export function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const [separator, setSeparator] = useState("");
+  const [customSeparator, setCustomSeparator] = useState("");
   const [wrapIn, setWrapIn] = useState("");
 
   const [textarea1, setTextarea1] = useState("");
@@ -30,10 +33,28 @@ export function Home() {
     const arrayTextarea2 = textarea2.split("\n");
     const arrayTextarea3 = textarea3.split("\n");
 
-    // setTextareaMerged(textarea1);
-    console.log(arrayTextarea1);
-    console.log(arrayTextarea2);
-    console.log(arrayTextarea3);
+    const wrapperLeft =
+      wrapIn === "quotes" ? '"' : wrapIn === "brackets" ? "[" : "";
+    const wrapperRight =
+      wrapIn === "quotes" ? '"' : wrapIn === "brackets" ? "]" : "";
+
+    const wordsMerged = arrayTextarea1
+      .map((text1) =>
+        arrayTextarea2.map((text2) =>
+          arrayTextarea3.map(
+            (text3) =>
+              `${wrapperLeft}${separator === "+" ? separator : ""}${text1}${
+                separator === "%" ? customSeparator : separator
+              }${text2}${
+                separator === "%" ? customSeparator : separator
+              }${text3}${wrapperRight}`
+          )
+        )
+      )
+      .flat(Infinity)
+      .join("\n");
+
+    setTextareaMerged(wordsMerged);
   };
 
   return (
@@ -51,28 +72,64 @@ export function Home() {
           <div className={styles.optionsBox}>
             <p>Separador:</p>
             <div className={styles.buttonGroup}>
-              <ButtonOptions isSelect={separator === ""} text="Nada" handleClick={() => setSeparator("")} />
-              <ButtonOptions isSelect={separator === " "}
+              <ButtonOptions
+                isSelect={separator === ""}
+                text="Nada"
+                handleClick={() => setSeparator("")}
+              />
+              <ButtonOptions
+                isSelect={separator === " "}
                 text="Espaço"
                 handleClick={() => setSeparator(" ")}
               />
-              <ButtonOptions isSelect={separator === "+"} text="+" handleClick={() => setSeparator("+")} />
-              <ButtonOptions isSelect={separator === "-"} text="-" handleClick={() => setSeparator("-")} />
-              <div>
-                <ButtonOptions isSelect={separator === "%"}
-                  text="Outro"
-                  handleClick={() => setSeparator("%")}
+              <ButtonOptions
+                isSelect={separator === "+"}
+                text="+"
+                handleClick={() => setSeparator("+")}
+              />
+              <ButtonOptions
+                isSelect={separator === "-"}
+                text="-"
+                handleClick={() => setSeparator("-")}
+              />
+              <ButtonOptions
+                isSelect={separator === "%"}
+                text="Outro"
+                handleClick={() => {
+                  setSeparator("%");
+                  setCustomSeparator("");
+                  setTimeout(() => {
+                    inputRef.current?.focus();
+                  }, 200);
+                }}
+              />
+              {separator === "%" && (
+                <input
+                  ref={inputRef}
+                  type="text"
+                  onChange={(e) => setCustomSeparator(e.target.value)}
                 />
-                {separator === "%" && <input type="text" name="" id="" />}
-              </div>
+              )}
             </div>
           </div>
           <div className={styles.optionsBox}>
             <p>Cercar por:</p>
             <div className={styles.buttonGroup}>
-              <ButtonOptions isSelect={wrapIn === ""} text="Nada" handleClick={() => setWrapIn("")} />
-              <ButtonOptions isSelect={wrapIn === "quotes"} text={`" "`} handleClick={() => setWrapIn("quotes")} />
-              <ButtonOptions isSelect={wrapIn === "brackets"} text={`[ ]`} handleClick={() => setWrapIn("brackets")} />
+              <ButtonOptions
+                isSelect={wrapIn === ""}
+                text="Nada"
+                handleClick={() => setWrapIn("")}
+              />
+              <ButtonOptions
+                isSelect={wrapIn === "quotes"}
+                text={`" "`}
+                handleClick={() => setWrapIn("quotes")}
+              />
+              <ButtonOptions
+                isSelect={wrapIn === "brackets"}
+                text={`[ ]`}
+                handleClick={() => setWrapIn("brackets")}
+              />
             </div>
           </div>
         </section>
