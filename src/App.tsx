566a99605ca54237fc850f714b61c4c646922602
Pedro Paulo <pedropaulotj@gmail.com>
2022-08-30 15:18:34 -0300

    fix: Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Home } from './pages/home'
+import React, { useEffect } from "react";
+import { Home } from "./pages/home";
 
 function App() {
-  return (
-    <Home/>
-  )
+  useEffect(() => {
+    function ReactIsInDevelomentMode() {
+      return "_self" in React.createElement("div");
+    }
+    console.log("IsDev? ", ReactIsInDevelomentMode());
+  }, []);
+  return <Home />;
 }
 
-export default App
+export default App;
